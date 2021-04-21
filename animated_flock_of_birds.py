4ffcfa414d5f055083ea183a5de666e7fe132d4e
DykunXV <dykunxv@gmail.com>
2021-04-21 20:08:50 +0200

    first commit

---
+++ animated_flock_of_birds.py
@@ -0,0 +1,13 @@
+bl_info = {
+    "name": "Animated flock of birds",
+    "blender": (2, 9, 0),
+    "category": "Object",
+}
+def register():
+    print("Hello World")
+def unregister():
+    print("Goodbye World")
+
+import bpy
+import typing
+import math
